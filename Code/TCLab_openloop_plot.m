bc86c6a0913b30e8bffa0c83ffd11ad1e0ec33ea
Ricardo Francisco <ricardo.n.francisco@tecnico.ulisboa.pt>
2026-05-05 00:34:43 +0100

    Finished model identification best model = n=9

---
+++ Code/TCLab_openloop_plot.m
@@ -0,0 +1,26 @@
+clear all
+close all
+clc    
+
+load('matfiles/openloop_data_1.mat','y','u','t');
+load('matfiles/openloop_data_2.mat','y','u','t');
+load('matfiles/openloop_data_3.mat','y','u','t');
+
+figure
+theme(figure,"light")
+set(gcf, 'DefaultAxesFontSize', 24) 
+set(gcf, 'DefaultTextFontSize', 24)
+subplot(2,1,1), hold on, grid on   
+plot(t,y(1,:),'.','MarkerSize',12)
+plot(t,y(2,:),'.','MarkerSize',12)
+legend('Temperature 1','Temperature 2','Location','best')
+xlabel('Time [s]')
+ylabel('Temperature [°C]')
+subplot(2,1,2), hold on, grid on   
+width = 5;
+stairs(t,u(1,:),'LineWidth',width)
+stairs(t,u(2,:),'LineWidth',width)
+legend('Heater 1','Heater 2','Location','best')
+xlabel('Time [s]')
+ylabel('Heater control [%]')
+ylim([0 100]);
